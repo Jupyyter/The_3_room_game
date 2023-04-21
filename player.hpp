cdeb0be42469cdb4dc5f87ef935ee6c4894e649d
23blol <77333954+lexu-html@users.noreply.github.com>
2023-04-21 23:21:23 +0300

    Update player.hpp

--- player.hpp
+++ player.hpp
@@ -2,7 +2,7 @@
 
 class Player : public Sprite{
     public:
-    Player(std::string name, RenderWindow window, Vector2 pos);
+    Player(std::string name, RenderWindow window, Vector2 pos, float scaler = 1.0f);
     //public part of the stuff that deals with player movement
     void Travel(int x, int y, uint32_t m);//basically used when you have the initiative of moving to a tile
     void Move();//and this is used because travel is only executed when you press a button but after you press that button the player has to move 32 pixels without needing you to press anything
@@ -17,7 +17,7 @@ class Player : public Sprite{
     bool moving, shouldmove;//obviously used for the characters movement 
     std::chrono::steady_clock::time_point Timep;//timepoint used to make the characters walk smoothly instead of just teleporting an entire tile when you press a button
     int lx,ly,nx,ny;//keeping track of his whereabouts
-    uint32_t milliseconds;//keeping track of how much time he needs to move 1 tile
+    uint32_t milliseconds;//keeping treack of how much time he needs to move 1 tile
 
     //private part of the stuff that deals with player animation and stuff
     SDL_Rect srcRect;
