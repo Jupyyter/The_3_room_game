478513864dc8444c832bd8471b4a8a5f413c2a8b
23blol <77333954+lexu-html@users.noreply.github.com>
2023-04-26 13:14:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,44 +25,42 @@ int main(){
     // Load the sprite
     Sprite superidol("superIdol.jpg", window);
     Sound supersound("super-idol.wav");
-    Player Unt("hehim.png", window, Vector2(0,0));
-    level a("untitled.json", window);
+    Player Unt("hehim.png", window, Vector2(0,0), 2.3f);
+    level a("untitled.json", window, 2.3f);
     Vector2 suppos;
 
     //Main loop
     while (window.run())
     {
-        int pltx = Unt.GetPos().x/32;
-        int plty = Unt.GetPos().y/32;
-        if(window.keyPressed('a') && !a.mapBounds[plty][pltx-1]){
+        if(window.keyPressed('a')){
             Unt.Travel(-1,0, 750);
         }
-        if(window.keyPressed('d') && !a.mapBounds[plty][pltx+1]){
+        if(window.keyPressed('d')){
             Unt.Travel(1,0, 750);
         }
-        if(window.keyPressed('w') && !a.mapBounds[plty-1][pltx]){
+        if(window.keyPressed('w')){
             Unt.Travel(0,-1, 750); 
         }
-        if(window.keyPressed('s') && !a.mapBounds[plty+1][pltx]){
+        if(window.keyPressed('s')){
             Unt.Travel(0,1, 750);
         }
 
         
-        /*
-        float distance = sqrt((Unt.pos.x - suppos.x)*(Unt.pos.x - suppos.x) + (Unt.pos.y - suppos.y)*(Unt.pos.y - suppos.y));
+        Vector2 ppos = Unt.GetPos();
+        float distance = sqrt((ppos.x - suppos.x)*(ppos.x - suppos.x) + (ppos.y - suppos.y)*(ppos.y - suppos.y));
 
         if(distance <= 1000){
             supersound.SetVolume(128/1000.0f*(1000-distance));
             supersound.PlaySync();
         }
-        */
         
-        Unt.Move();
+        
+        Unt.Move(a.mapBounds);
         
         window.clear();
         a.renderLVL(window);
         Unt.Draw(window);
-        //window.render(superidol, suppos.x, suppos.y);
+        window.render(superidol, suppos.x, suppos.y);
         window.renderText("I am here", 255,255,255, 100, 200);
         window.display();
     }
