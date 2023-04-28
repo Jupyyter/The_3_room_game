e5d278e95c0aac33d019e7b6214d9aff2b7c8690
Jupyyter <92679827+Jupyyter@users.noreply.github.com>
2023-04-29 02:12:59 +0300

    Update level.hpp

--- level.hpp
+++ level.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+struct properties
+{
+    bool collision=false;
+    std::string text="";
+    bool secondLayer=false;
+};
+
 struct texture
 {
-    bool collider = false;
     int refS = 0;
     SDL_Rect srcRect;
+    properties Properties;
     texture(){};
-    texture(int refS, SDL_Rect srcRect, bool p_collider)
-        : refS(refS), srcRect(srcRect), collider(p_collider)
+    texture(int refS, SDL_Rect srcRect, properties p_properties)
+        : refS(refS), srcRect(srcRect), Properties(p_properties)
     {
     }
 
@@ -28,17 +35,19 @@ class level
 {
 public:
     std::map<int, std::map<int, bool>> mapBounds;
+    std::map<int, std::map<int, std::string>> interactible;
     // load level
-    level(const std::string &name, RenderWindow &window);
+    level(const std::string &name, RenderWindow &window,int scaler);
     // render level :)
     void renderLVL(RenderWindow &window);
+    void renderLVL2(RenderWindow &window);
 
 private:
     // saves the tiles position and size for it to be later read and drawn
-    void defineTiles();
+    void defineTiles(int scaler);
     // saving all the sprites within the map as textures
     void defineTextures(RenderWindow &window);
-    std::map<int, bool> isDynamicTileSet;
+    std::map<int, std::map<int, bool>> secondLayer;
     std::map<int, std::map<int, texture>> spriteSheetTextures;
     std::map<int, std::map<int, tile>> allTiles;
     std::vector<Sprite> alltilesprite;
