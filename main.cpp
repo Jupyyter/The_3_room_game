e36e46b28261c3e79d403a6763fb62d09737b8bc
Jupyyter <92679827+Jupyyter@users.noreply.github.com>
2023-06-08 00:11:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,87 @@
 #define SDL_MAIN_HANDLED
-#include "sldlib.hpp"
+#include "sdlLib.hpp"
 
-#define SCREEN_WIDTH 1280 
+#define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
-//this will need further encapsulation
-bool Collide(Sprite& a, Sprite& b, Vector2& ap, Vector2& bp){
+// this will need further encapsulation
+bool Collide(Sprite &a, Sprite &b, vector2 &ap, vector2 &bp)
+{
     return (ap.x + a.width >= bp.x && bp.x + b.width >= ap.x && ap.y + a.height >= bp.y && bp.y + b.height >= ap.y);
 }
 
-//to do list:
-//encapsulate SDL_Rect better
-//draw the level using a function that is part of the window class
-//maybe remove texture (seems unnecessary)
+// to do list:
+// encapsulate SDL_Rect better
+// draw the level using a function that is part of the window class
+// maybe remove texture (seems unnecessary)
 
-int main(){
-    //init library
+int main()
+{
+    // init library
     RenderWindow::InitAll();
 
-    //create window
+    // create window
     RenderWindow window("MARIOANA EROINA COCAINA CRACK", 1280, 720);
     window.setFps(60);
 
     // Load the sprite
-    Sprite superidol("superIdol.jpg", window);
-    Sound supersound("super-idol.wav");
-    Player Unt("hehim.png", window, Vector2(0,0), 2.3f);
-    level a("untitled.json", window, 2.3f);
-    Vector2 suppos;
+    Player Unt("E:/Aseprite/saved/allPablo.png", window, vector2(512, 512), 2);
+    level a("untitled3.json", window, 2);
+    vector2 suppos;
 
-    //Main loop
+    // Main loop
     while (window.run())
     {
-        if(window.keyPressed('a')){
-            Unt.Travel(-1,0, 750);
-        }
-        if(window.keyPressed('d')){
-            Unt.Travel(1,0, 750);
-        }
-        if(window.keyPressed('w')){
-            Unt.Travel(0,-1, 750); 
-        }
-        if(window.keyPressed('s')){
-            Unt.Travel(0,1, 750);
+        //Unt.Move();
+        // Get the current state of the keyboard
+        const Uint8 *keyboardState = SDL_GetKeyboardState(NULL);
+
+        // Check the state of the arrow keys and w a s d
+        if (!Unt.interacting)
+        {
+            if (keyboardState[SDL_SCANCODE_LEFT] || window.keyPressed('a'))
+            {
+                Unt.Travel(-1, 0, 500, 100,a.mapBounds);
+            }
+            else if (keyboardState[SDL_SCANCODE_RIGHT] || window.keyPressed('d'))
+            {
+                Unt.Travel(1, 0, 500,100,a.mapBounds);
+            }
+            else if (keyboardState[SDL_SCANCODE_UP] || window.keyPressed('w'))
+            {
+                Unt.Travel(0, -1, 500,100,a.mapBounds);
+            }
+            else if (keyboardState[SDL_SCANCODE_DOWN] || window.keyPressed('s'))
+            {
+                Unt.Travel(0, 1, 500,100,a.mapBounds);
+            }
         }
 
-        
-        Vector2 ppos = Unt.GetPos();
-        float distance = sqrt((ppos.x - suppos.x)*(ppos.x - suppos.x) + (ppos.y - suppos.y)*(ppos.y - suppos.y));
+        vector2 ppos = Unt.GetPos();
+        float distance = sqrt((ppos.x - suppos.x) * (ppos.x - suppos.x) + (ppos.y - suppos.y) * (ppos.y - suppos.y));
+        Unt.Move();
 
-        if(distance <= 1000){
-            supersound.SetVolume(128/1000.0f*(1000-distance));
-            supersound.PlaySync();
-        }
-        
-        
-        Unt.Move(a.mapBounds);
-        
         window.clear();
+        // Unt.interact(a.interactible,window);
         a.renderLVL(window);
         Unt.Draw(window);
-        window.render(superidol, suppos.x, suppos.y);
-        window.renderText("I am here", 255,255,255, 100, 200);
+        a.renderLVL2(window);
+
+        if (window.keyPressedDown(SDL_SCANCODE_Z) && Unt.frontObject(a.interactible, window))
+        {
+            Unt.interacting = !Unt.interacting;
+        }
+
+        if (Unt.interacting)
+        {
+            Unt.interact(a.interactible, window,200);
+        }
+
+        // window.renderText("I am here", 255,255,255, 100, 200);
         window.display();
     }
-    
-    //Clean up
+
+    // Clean up
     RenderWindow::QuitAll();
     return 0;
 }
-
