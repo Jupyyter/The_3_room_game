ea7a9bfcc186cf7c9a32162517e95d5e2d0d994b
Jupyyter <92679827+Jupyyter@users.noreply.github.com>
2023-06-07 23:24:50 +0300

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,6 +1,6 @@
 #include "sdlLib.hpp"
 
-Player::Player(std::string name, RenderWindow window, vector2 pos, float scaler) : Sprite(name, window), moving(false), shouldmove(false)
+Player::Player(std::string name, RenderWindow window, vector2 pos, float scaler) : Sprite(name, window), moving(false), shouldmove(false), textindex(0)
 {
     this->pos.x = pos.x;
     this->pos.y = pos.y;
@@ -13,17 +13,63 @@ Player::Player(std::string name, RenderWindow window, vector2 pos, float scaler)
     this->srcRect.h = 32;
 }
 
+void Player::interact(std::map<int, std::map<int, std::string>> interactible, RenderWindow window, int ms)
+{
+    if (!moving)
+    {
+        std::string intertext = interactible[this->pos.y / this->pos.h + this->ny][this->pos.x / this->pos.w + this->nx];
+
+        if (intertext != this->lasttext)
+        {
+            this->textindex = 0;
+            this->lasttext = intertext;
+            this->ttp = std::chrono::steady_clock::now();
+        }
+        else if (intertext.size() > 0)
+        {
+            for (int i = 0; i <= this->textindex; i++)
+            {
+                char letter = intertext[i];
+                std::string finaltext(1, letter);
+                window.renderText(finaltext, 255, 255, 255, 100 + i * window.getFontWidth(), 100);
+            }
+            long long duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - this->ttp).count();
+            if (duration >= ms)
+            {
+                if (this->textindex < intertext.size() - 1)
+                {
+                    this->textindex++;
+                }
+                this->ttp = std::chrono::steady_clock::now();
+            }
+        }
+    }
+}
+
+bool Player::frontObject(std::map<int, std::map<int, std::string>> interactible, RenderWindow window)
+{
+    if (interactible[this->pos.y / this->pos.h + this->ny][this->pos.x / this->pos.w + this->nx] == "")
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 vector2 Player::GetPos()
 {
     return vector2(this->pos.x, this->pos.y);
 }
 
-void Player::Move(std::map<int, std::map<int, bool>> mapBounds)
+void Player::Move()
 {
-    if (shouldmove && !mapBounds[posY2 / this->pos.w + this->ny][this->posX2 / this->pos.h + this->nx])
+    if (shouldmove)
     {
         if (!moving)
         {
+            // this is preparing for the movement itself
             this->Timep = std::chrono::steady_clock::now();
             this->lx = int(this->pos.x);
             this->ly = int(this->pos.y);
@@ -33,51 +79,61 @@ void Player::Move(std::map<int, std::map<int, bool>> mapBounds)
         {
             auto end = std::chrono::steady_clock::now();
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - this->Timep).count();
+            static auto animationDuration = 0;
             float ratio = float(duration) / milliseconds;
             if (ratio <= 1.0f)
             {
                 this->pos.x = lx + nx * this->pos.w * ratio;
                 this->pos.y = ly + ny * this->pos.h * ratio;
 
-                if (duration > milliseconds / 4.0f * (this->srcRect.x / 32 + 1))
-                {
-                    srcRect.x += 32;
-                }
+                srcRect.x = 32 * (((animationDuration + duration) / this->animSpeed) % 4);
+                std::cout << (((animationDuration + duration) / this->animSpeed) % 4) << " ";
             }
             else
             {
                 this->pos.x = lx + nx * this->pos.w;
                 this->pos.y = ly + ny * this->pos.h;
                 this->moving = false;
-                posX2 = pos.x;
-                posY2 = pos.y;
                 this->shouldmove = false;
                 // animation related shenanigans
                 srcRect.x = 0;
+                if (moveInput)
+                {
+                    Move();
+                    animationDuration += duration;
+                    srcRect.x = 32 * (((animationDuration) / this->animSpeed) % 4);
+                }
+                else
+                {
+                    animationDuration = 0;
+                }
             }
         }
     }
-}
-void Player::interact(std::map<int, std::map<int, std::string>> interactible, RenderWindow window)
-{
-    window.renderText(interactible[posY2 / this->pos.w + this->ny][this->posX2 / this->pos.h + this->nx], 255,255,255, 100, 100);
+    moveInput = false;
 }
 
-void Player::Travel(int x, int y, uint32_t m)
+void Player::Travel(int x, int y, uint32_t m, uint32_t p_animSpeed, std::map<int, std::map<int, bool>> mapBounds)
 {
+    if (!mapBounds[this->pos.y / this->pos.h + this->ny][this->pos.x / this->pos.w + this->nx])
+    {
+        moveInput = true;
+    }
     if (!moving)
     {
-        this->shouldmove = true;
         this->nx = x;
         this->ny = y;
-        posX2 = pos.x;
-        posY2 = pos.y;
-        this->milliseconds = m;
+        if (!mapBounds[this->pos.y / this->pos.h + this->ny][this->pos.x / this->pos.w + this->nx])
+        {
+            this->shouldmove = true;
+            this->milliseconds = m;
+            this->animSpeed = p_animSpeed;
+        }
 
         // this part deals with animation (d u r l)
-        if (nx == 0)
+        if (x == 0)
         { // this means I am moving on the y axis
-            if (ny == 1)
+            if (y == 1)
             { // down
                 this->srcRect.y = 0;
             }
@@ -88,7 +144,7 @@ void Player::Travel(int x, int y, uint32_t m)
         }
         else
         { // this means I am moving in the x axis
-            if (nx == 1)
+            if (x == 1)
             { // right
                 this->srcRect.y = 64;
             }
@@ -102,7 +158,7 @@ void Player::Travel(int x, int y, uint32_t m)
 
 void Player::Draw(RenderWindow wind)
 {
-    SDL_Rect srcRect2=pos;
-    srcRect2.y-=32;
+    SDL_Rect srcRect2 = pos;
+    srcRect2.y -= 32;
     wind.render(*this, this->srcRect, srcRect2);
 }
